17ce3037110d292507f569fd840e51332a064874
bocsir <jacobwharris13@gmail.com>
2023-12-29 03:12:08 -0500

    more dry, cool background

---
+++ script.js
@@ -0,0 +1,16 @@
+const tween = KUTE.fromTo(
+    '#blob1',
+    { path: '#blob1' },
+    { path: '#blob2' },
+    { repeat: 999, duration: 3000, yoyo: true }
+)
+
+tween.start();
+
+document.addEventListener("DOMContentLoaded", function () {
+    fetch("navbar.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("navbar").innerHTML = data;
+        });
+});
